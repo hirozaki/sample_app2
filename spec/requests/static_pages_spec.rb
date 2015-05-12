ef95a2ad15a0219375e36ae3bc9e092b7716bd1a
hirozaki <bluecut22@gmail.com>
2015-05-12 21:50:52 +0900

    Finish?

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,12 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App2'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App2')
     end
     
     it "should have the title 'Home'" do 
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App2")
     end 
   end
@@ -19,12 +19,12 @@ describe "Static pages" do
   describe "Help page" do 
 
 
-  it "should have the content 'Help'"  do
-  	visit '/static_pages/help'
+  it "should have h1 'Help'"  do
+  	visit help_path
     expect(page).to have_content('Help')
   end
   it "should have the title 'Help'"  do  
-    visit '/static_pages/help'
+    visit help_path
     expect(page).to have_title("| Help")
   end 
 end
@@ -32,13 +32,13 @@ end
  describe "About page" do 
 
 
-   it "should have the content 'About Us'"  do 
-     visit '/static_pages/about'
+   it "should have h1 'About Us'"  do 
+     visit about_path
      expect(page).to have_content('About Us')
    end
    
    it "should have the title 'About Us'"  do 
-     visit '/static_pages/about'
+     visit about_path
      expect(page).to have_title("| About Us")
    end
   end
@@ -46,13 +46,13 @@ end
   describe "Contact page" do
 
     it "should have the content 'Contact'" do 
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact' 
-      expect(page).to have_title("| Contact")
+      visit contact_path 
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App2 | Contact")
     end
   end
 end
