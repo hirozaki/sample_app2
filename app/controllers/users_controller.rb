5384f07516156978465bfe203c8f2abe98ef8a9e
hirozaki <bluecut22@gmail.com>
2015-06-05 20:00:14 +0900

    almost finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   	def create
   		@user = User.new(user_params)
   		if @user.save
+         sign_in @user
          flash[:success] = "Welcome to the Sample App2!"
     		 redirect_to @user
       else
---
+++ vendor/bundle/ruby/2.0.0/gems/gherkin-2.12.2/ext/gherkin_lexer_lu/siteconf20150605-13913-15uos8m.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150605-13913-1hs9kun"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
