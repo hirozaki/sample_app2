425eef779b2593c233e6907d8550400bf76f996f
hirozaki <bluecut22@gmail.com>
2015-06-24 07:49:37 +0900

    Finih user edit, update, destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,10 +17,20 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:admin) }
+
 	it { should be_valid }
+	it { should_not be_admin }
 
 
-	
+	describe "with admin attribute set to 'true'"  do 
+	  before do 
+	    @user.save!
+	    @user.toggle!(:admin)
+	  end
+
+	  it { should be_admin }
+	end
 
 
 
@@ -105,7 +115,7 @@ describe User do
 			before { @user.save }
 			its(:remember_token) { should_not be_blank }
 		end
-	end
+end
 
 
 
